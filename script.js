4d6cae82107d198fe99986fc70b7165f0d019a13
Ryan Arnold <rja87.sd@gmail.com>
2024-02-12 21:16:17 -0600

    Add styling in style.css. Add invocations to script.js to test code. Add content to index.html.

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ Television.prototype.changePower = function () {
 
 Television.prototype.changeChannel = function (newChannel) {
   this.currentChannel = newChannel;
+  console.log(`The channel is now set to ${this.currentChannel}.`);
 };
 
-Television.prototype.getState = function () {
+Television.prototype.watchTV = function () {
   if (!this.powerStatus) {
     console.log("Please turn on the TV.");
     return;
@@ -25,12 +26,33 @@ Television.prototype.getState = function () {
     console.log("Please enter a valid channel number (1 - 100).");
     return;
   } else {
-    console.log(`The TV is ${this.powerStatus ? "ON" : "OFF"}.`);
-    console.log(`The TV is set to channel ${this.currentChannel}.`);
+    console.log(`Watching TV on channel ${this.currentChannel}...`);
     return;
   }
 };
 
+Television.prototype.getState = function () {
+  console.log(`The TV is ${this.powerStatus ? "ON" : "OFF"}.`);
+  console.log(`The TV is set to channel ${this.currentChannel}.`);
+};
+
 const myTelevision1 = new Television(false, 12);
-const myCoffeeMaker2 = new CoffeeMaker(true, 33);
-const myCoffeeMaker3 = new CoffeeMaker(false, 115);
+const myTelevision2 = new Television(true, 33);
+const myTelevision3 = new Television(false, 115);
+
+myTelevision1.getState();
+myTelevision1.watchTV();
+myTelevision1.changePower();
+myTelevision1.watchTV();
+
+myTelevision2.getState();
+myTelevision2.watchTV();
+myTelevision2.changeChannel(30);
+myTelevision2.watchTV();
+
+myTelevision3.getState();
+myTelevision3.watchTV();
+myTelevision3.changePower();
+myTelevision3.watchTV();
+myTelevision3.changeChannel(99);
+myTelevision3.watchTV();
